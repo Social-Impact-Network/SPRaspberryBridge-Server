2ece84b4c09237850464cc5f7778997ccc4daf5b
daniel-pan-moos <daniel_pan_moos@protonmail.com>
2020-09-14 20:12:49 +0200

    new approach

---
+++ models/device.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const deviceSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    timestamp: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    energyOutput: {
+        type: Number,
+        required: true,
+        default: 0
+    }
+});
+
+module.exports = mongoose.model('Subscriber', deviceSchema);
---
+++ routes/devices.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const router = express.Router();
+const Device = require('../models/device')
+
+// Getting all
+router.get('/', async (req, res) => {
+    try {
+        const device = await Device.find();
+        res.json(device)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+});
+// Getting one
+router.get('/:id', getDevice, (req, res) => {
+    // res.send(res.device.name);
+    res.json(res.device);
+});
+// Creating one
+router.post('/', async (req, res) => {
+    const device = new Device({
+        name: req.body.name,
+        location:
+        req.body.location,
+        "energyOutput": req.body.energyOutput
+    });
+
+    try {
+        const newDevice = await device.save();
+        res.status(201).json(newDevice);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+// Updating one
+router.patch('/:id', getDevice, async (req, res) => {
+    if (req.body.name) {
+        res.device.name = req.body.name;
+        
+    }    
+    if (req.body.location) {
+        res.device.name = req.body.energyOutput;
+
+    }  
+    try {
+        const updatedDevice = await res.device.save();
+        res.json(updatedDevice)
+    }  catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+});
+// Deleting one
+router.delete('/:id', getDevice, async (req, res) => {
+    try {
+        await res.device.remove();
+        res.json({ message: 'Deleted Device' });
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+});
+
+// middleware
+async function getDevice(req, res, next) {
+    let device;
+    try {
+        device = await Device.findById(req.params.id);
+        if (device == null) {
+            return res.status(404).json({ message: 'Cannot find device' });
+        }
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+
+    res.device = device;
+    next();
+}
+
+module.exports = router;

e7f618191e0d8022acd01f9b3e66356e8c1fc66d
daniel-pan-moos <daniel_pan_moos@protonmail.com>
2020-09-29 17:55:03 +0200

    minor refactoring

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
+const db = mongoose.connection;
 
 // write in a .env-file (same level as server.js, package.json and so on) 
 // following:
 // DATABASE_URL=yourDatabaseURL
-mongoose.connect(process.env.DATABASE_URL, 
-    { 
+mongoose.connect(process.env.DATABASE_URL, { 
         useNewUrlParser: true, 
         useUnifiedTopology: true 
-    });
-const db = mongoose.connection;
+    }
+);
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Successfully connected to database'));
 app.use(express.json());
